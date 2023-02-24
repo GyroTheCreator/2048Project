7fc76f0518fe1da5fbd133717b574288959067e1
Gyro <blindsobroken@gmail.com>
2023-02-24 11:41:55 +0100

    Ajout et réglage de bug

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ import random
 # VARIABLES #
 #############
 
-grid = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,2,2]]
+grid = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
 grid_values = [[None,None,None,None],[None,None,None,None],[None,None,None,None],[None,None,None,None]]
 
 grid_width = 100
@@ -58,6 +58,8 @@ window = Tk()
 w = 500
 h = 630
 
+right = 0
+
 screenwidth = window.winfo_screenwidth()
 screenheight = window.winfo_screenheight()
 
@@ -74,20 +76,122 @@ window.geometry('%dx%d+%d+%d' % (w, h, x, y))
 window.iconbitmap("media/icon.ico")
 window.config(bg=colors["background"])
 
+##########################
+# DESIGN AIDE PAR CARLOS # ★
+##########################
+
+# DESIGN FRAME
+max_space = Frame(window, background=colors["background"], height=35, width=400)
+max_frame = Frame(window, background=colors["background"])
+
+title_frame = Frame(max_frame, background=colors["background"])
+title_space = Frame(max_frame, background=colors["background"], width=40)
+
+score_frame = Frame(max_frame, background=colors["background_ampli"])
+score_space = Frame(max_frame, background=colors["background"], width=10)
+
+max_score_frame = Frame(max_frame, background=colors["background_ampli"])
+max_bottom_frame = Frame(window, background=colors["background"])
+
+label_frame = Frame(max_bottom_frame, background=colors["background"])
+button_frame = Frame(max_bottom_frame, background=colors["background"])
+
+# DESIGN LABEL
+logo_label = Label(title_frame, text="2048", font="Arial, 54", fg="white", background=colors["background"])
+slide_label = Label(label_frame, text="Slide the numbers to get 8192 in order to win !", font="Arial, 12", background=colors["background"], fg="white")
+
+score_label = Label(max_score_frame, text="          Score         ", fg="white", background=colors["background_ampli"])
+value_score_label = Label(max_score_frame, text=f"         {score}        ", fg="white", background=colors["background_ampli"])
+
+top_label = Label(score_frame, text=f"      Max ★      ", fg="white", background=colors["background_ampli"])
+value_top_label = Label(score_frame, text=f"        {maxscore}         ", fg="white", background=colors["background_ampli"])
+
+# DESIGN BUTTON
+
+# PACK ALL THIS
+max_space.pack()
+max_frame.pack()
+
+title_frame.pack(side=LEFT)
+title_space.pack(side=LEFT)
+
+score_frame.pack(side=RIGHT)
+score_space.pack(side=RIGHT)
+
+max_score_frame.pack(side=RIGHT)
+max_bottom_frame.pack(pady=10)
+
+score_label.pack(pady=1)
+value_score_label.pack(pady=1)
+
+top_label.pack(pady=1, padx=5)
+value_top_label.pack(pady=1, padx=5)
+
+label_frame.pack(side=LEFT)
+button_frame.pack(side=RIGHT)
+
+logo_label.pack()
+slide_label.pack()
+
+# SPACE BETWEEN THE GAME
+between = Frame(window, height=20, background=colors["background"])
+between.pack()
+
+# BLOCK FRAME
+alls = Frame(window, height=400, width=415, background=colors["background_ampli"])
+alls.pack()
+
+for i in range(len(grid)):
+    for j in range(len(grid[i])):
+        grid_values[i][j] = Label(alls, text=grid[i][j], width=8, height=3, borderwidth=1, relief="solid",
+                                  font=("Comic Sans MS", 14), fg="white", bg=colors[grid[i][j]])
+        grid_values[i][j].place(x=10 + grid_width * j, y=25 + grid_height * i)
+
 #############
 # MAIN CODE #
 #############
 
 # GRID DISPLAY
 def display_grid():
-    global new_button
-
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            grid_values[i][j] = Label(alls, text=grid[i][j], width=8, height=3, borderwidth=1, relief="solid",
-                                      font=("Comic Sans MS", 14), fg="white", bg=colors[grid[i][j]])
-            grid_values[i][j].place(x=10 + grid_width * j, y=25 + grid_height * i)
-    new_button.config(state=DISABLED)
+            if grid[i][j] == 0:
+                grid_values[i][j].config(text="", bg=colors["background"])
+            else:
+                grid_values[i][j].config(text=grid[i][j], bg=colors[grid[i][j]])
+
+def new_game():
+    global grid
+    grid = [[0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0],
+            [0,0,0,0]]
+
+    [random_spawn() for _ in range(2)]
+    display_grid()
+
+new_button = Button(button_frame, text="New game", height=1, width=8, command=new_game)
+new_button.pack(padx=20)
+
+def random_spawn():
+    global grid, random_caseh, random,casew
+
+    random.caseh = random.randint(0,3)
+    random.casew = random.randint(0,3)
+
+    if grid[random.caseh][random.casew] != 0:
+        while grid[random.caseh][random.casew] > 0:
+            random.caseh = random.randint(0, 3)
+            random.casew = random.randint(0, 3)
+        else:
+            grid[random.caseh][random.casew] = 2
+    else:
+        grid[random.caseh][random.casew] = 2
+
+[random_spawn() for _ in range(2)]
+display_grid()
+
+#[random_spawn() for _ in range(2)]
 
 # PACK FUNCTION
 def pack(a, b, c, d):
@@ -121,39 +225,56 @@ def pack(a, b, c, d):
         value_score_label.config(text=f"{score+random.randint(5,20)}") # Add score in the tab
         nmove += 1 # Add +1 to movement list
 
-    temp=[a,b,c,d] # Temporary list for stockage
-    return temp
+    if right == 0:
+        temp=[a,b,c,d] # Temporary list for stockage
+        return temp
+    elif right == 1:
+        temp2=[d,c,b,a]
+        return temp2
+
 
 # FUNCTION FOR MOVE LEFT ACTION
 def move_left(event):
+    global right
+    right = 0
     grid[0] = pack(grid[0][0], grid[0][1], grid[0][2], grid[0][3])
     grid[1] = pack(grid[1][0], grid[1][1], grid[1][2], grid[1][3])
     grid[2] = pack(grid[2][0], grid[2][1], grid[2][2], grid[2][3])
     grid[3] = pack(grid[3][0], grid[3][1], grid[3][2], grid[3][3])
+    random_spawn()
     display_grid() # Refresh the game
 
 # FUNCTION FOR MOVE RIGHT ACTION
 def move_right(event):
-    grid[0] = pack(grid[0][3], grid[0][2], grid[0][1], grid[0][0])
-    grid[1] = pack(grid[1][3], grid[1][2], grid[1][1], grid[1][0])
-    grid[2] = pack(grid[2][3], grid[2][2], grid[2][1], grid[2][0])
-    grid[3] = pack(grid[3][3], grid[3][2], grid[3][1], grid[3][0])
+    global right
+    right = 1
+    grid[0] = pack(grid[0][0], grid[0][1], grid[0][2], grid[0][3])
+    grid[1] = pack(grid[1][0], grid[1][1], grid[1][2], grid[1][3])
+    grid[2] = pack(grid[2][0], grid[2][1], grid[2][2], grid[2][3])
+    grid[3] = pack(grid[3][0], grid[3][1], grid[3][2], grid[3][3])
+    random_spawn()
     display_grid() # Refresh the game
 
 # FUNCTION FOR MOVE UP ACTION
 def move_up(event):
+    global right
+    right = 0
     grid[0][0],grid[1][0],grid[2][0],grid[3][0] = pack(grid[0][0], grid[1][0], grid[2][0], grid[3][0])
     grid[0][1], grid[1][1], grid[2][1], grid[3][1] = pack(grid[0][1], grid[1][1], grid[2][1], grid[3][1])
     grid[0][2], grid[1][2], grid[2][2], grid[3][2] = pack(grid[0][2], grid[1][2], grid[2][2], grid[3][2])
     grid[0][3], grid[1][3], grid[2][3], grid[3][3] = pack(grid[0][3], grid[1][3], grid[2][3], grid[3][3])
+    random_spawn()
     display_grid() # Refresh the game
 
 # FUNCTION FOR MOVE DOWN ACTION
 def move_down(event):
-    grid[0][0],grid[1][0],grid[2][0],grid[3][0] = pack(grid[3][0], grid[2][0], grid[1][0], grid[0][0])
-    grid[0][1], grid[1][1], grid[2][1], grid[3][1] = pack(grid[3][1], grid[2][1], grid[1][1], grid[0][1])
-    grid[0][2], grid[1][2], grid[2][2], grid[3][2] = pack(grid[3][2], grid[2][2], grid[1][2], grid[0][2])
-    grid[0][3], grid[1][3], grid[2][3], grid[3][3] = pack(grid[3][3], grid[2][3], grid[1][3], grid[0][3])
+    global right
+    right = 1
+    grid[0][0],grid[1][0],grid[2][0],grid[3][0] = pack(grid[0][0], grid[1][0], grid[2][0], grid[3][0])
+    grid[0][1], grid[1][1], grid[2][1], grid[3][1] = pack(grid[0][1], grid[1][1], grid[2][1], grid[3][1])
+    grid[0][2], grid[1][2], grid[2][2], grid[3][2] = pack(grid[0][2], grid[1][2], grid[2][2], grid[3][2])
+    grid[0][3], grid[1][3], grid[2][3], grid[3][3] = pack(grid[0][3], grid[1][3], grid[2][3], grid[3][3])
+    random_spawn()
     display_grid() # Refresh the game
 
 # KEYBINDS
@@ -161,74 +282,6 @@ window.bind("<w>", move_up)
 window.bind("<s>", move_down)
 window.bind("<a>", move_left)
 window.bind("<d>", move_right)
-
-##########################
-# DESIGN AIDE PAR CARLOS # ★
-##########################
-
-# DESIGN FRAME
-max_space = Frame(window, background=colors["background"], height=35, width=400)
-max_frame = Frame(window, background=colors["background"])
-
-title_frame = Frame(max_frame, background=colors["background"])
-title_space = Frame(max_frame, background=colors["background"], width=40)
-
-score_frame = Frame(max_frame, background=colors["background_ampli"])
-score_space = Frame(max_frame, background=colors["background"], width=10)
-
-max_score_frame = Frame(max_frame, background=colors["background_ampli"])
-max_bottom_frame = Frame(window, background=colors["background"])
-
-label_frame = Frame(max_bottom_frame, background=colors["background"])
-button_frame = Frame(max_bottom_frame, background=colors["background"])
-
-# DESIGN LABEL
-logo_label = Label(title_frame, text="2048", font="Arial, 54", fg="white", background=colors["background"])
-slide_label = Label(label_frame, text="Slide the numbers to get 8192 in order to win !", font="Arial, 12", background=colors["background"], fg="white")
-
-score_label = Label(max_score_frame, text="          Score         ", fg="white", background=colors["background_ampli"])
-value_score_label = Label(max_score_frame, text=f"         {score}        ", fg="white", background=colors["background_ampli"])
-
-top_label = Label(score_frame, text=f"      Max ★      ", fg="white", background=colors["background_ampli"])
-value_top_label = Label(score_frame, text=f"        {maxscore}         ", fg="white", background=colors["background_ampli"])
-
-# DESIGN BUTTON
-new_button = Button(button_frame, text="New game", height=1, width=8, command=display_grid)
-
-# PACK ALL THIS
-max_space.pack()
-max_frame.pack()
-
-title_frame.pack(side=LEFT)
-title_space.pack(side=LEFT)
-
-score_frame.pack(side=RIGHT)
-score_space.pack(side=RIGHT)
-
-max_score_frame.pack(side=RIGHT)
-max_bottom_frame.pack(pady=10)
-
-score_label.pack(pady=1)
-value_score_label.pack(pady=1)
-
-top_label.pack(pady=1, padx=5)
-value_top_label.pack(pady=1, padx=5)
-
-label_frame.pack(side=LEFT)
-button_frame.pack(side=RIGHT)
-
-logo_label.pack()
-slide_label.pack()
-
-new_button.pack(padx=20)
-
-# SPACE BETWEEN THE GAME
-between = Frame(window, height=20, background=colors["background"])
-between.pack()
-
-# BLOCK FRAME
-alls = Frame(window, height=400, width=415, background=colors["background_ampli"])
-alls.pack()
 
 ##############
 # APP LAUNCH #
